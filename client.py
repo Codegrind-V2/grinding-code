fdec2764f4539266261bdbba62e919fcc3b0e381
vaibhav117 <vaibhavmathurcode@gmail.com>
2018-12-11 13:36:04 +0530

    google api

--- client.py
+++ client.py
@@ -16,7 +16,6 @@ def sendFile():
     fin = open('speech.raw', 'rb')
     files = {'file': fin} 
     requests.post('http://'+config.SERVER_ADDRESS+':'+config.SERVER_PORT+'/upload', files=files)
-  
     return 'success'
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request
 from werkzeug import secure_filename
 import os
+import io
 import config
 app = Flask(__name__)
 
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
 
 @app.route('/')
 def hello():
@@ -12,13 +16,39 @@ def hello():
 
 @app.route('/upload', methods = ['POST'])
 def upload_file():
-    #print(request)
     if request.method == 'POST':
-        #print(request)
         file = request.files['file']
         if file:
+            print("here")
             filename = secure_filename(file.filename)
             file.save(os.path.join(config.UPLOAD_LOC, filename))
+
+            # The name of the audio file to transcribe
+            file_name = os.path.join(
+                os.path.dirname(__file__),
+                    'files',
+                    filename)
+
+            client = speech.SpeechClient()
+            # Loads the audio into memory
+            with io.open(file_name, 'rb') as audio_file:
+                content = audio_file.read()
+                audio = types.RecognitionAudio(content=content)
+
+            config_google = types.RecognitionConfig(
+            encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+            sample_rate_hertz=44100,
+            language_code='en-US')
+
+            # Detects speech in the audio file
+            response1 = client.recognize(config_google, audio)
+
+            #print('response')
+            print(str(response1))
+            for result in response1.results:
+                print('Transcript: {}'.format(result.alternatives[0].transcript))
+
+            
             return "File Uploaded!"
         else:
             return "Error"
